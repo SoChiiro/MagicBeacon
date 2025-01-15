1231243f57e57c54f31896019f68d9350a5e6b61
SoChiiro <thomas.mariotte@efrei.net>
2025-01-15 17:20:22 +0100

    Début swagger pour authentification

---
+++ backend/api-docs/swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'User Authentication API',
+      version: '1.0.0',
+      description: 'API documentation for user authentication and profiles',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  apis: ['../backend/routes/authentificationRoutes.js', './routes/profileRoutes.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+module.exports = swaggerDocs;
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,35 +1,79 @@
+// // Import des librarires essentiels
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const userRoutes = require('../routes/authentificationRoutes');
+// const profileRoutes = require('../routes/profileRoutes');
+// const app = express();
+
+// require('dotenv').config({ path: '../backend/.env' });
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Connexion à MongoDB
+// const connectDB = () => {
+//     const mongoURI = process.env.MONGO_URI;
+
+//     if (!mongoURI) {
+//         console.error('Error: MONGO_URI is not defined in your .env file');
+//         process.exit(1);
+//     }
+//     mongoose.connect(process.env.MONGO_URI)
+//       .then(() => console.log(process.env.MONGO_URI))
+//       .then(() => console.log('MongoDB is connected'))
+//       .catch(err => console.log(err));
+//   };
+  
+
+// // connectDB()
+// app.use('/api/users', userRoutes);
+// app.use('/api/profile', profileRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// module.exports = connectDB;
+
 // Import des librarires essentiels
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('../routes/authentificationRoutes');
 const profileRoutes = require('../routes/profileRoutes');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const app = express();
+const swaggerDocs = require('../api-docs/swagger');
 
 require('dotenv').config({ path: '../backend/.env' });
 
+// Middleware de CORS et de parsing JSON
 app.use(cors());
 app.use(express.json());
 
 // Connexion à MongoDB
 const connectDB = () => {
-    const mongoURI = process.env.MONGO_URI;
+  const mongoURI = process.env.MONGO_URI;
 
-    if (!mongoURI) {
-        console.error('Error: MONGO_URI is not defined in your .env file');
-        process.exit(1);
-    }
-    mongoose.connect(process.env.MONGO_URI)
-      .then(() => console.log(process.env.MONGO_URI))
-      .then(() => console.log('MongoDB is connected'))
-      .catch(err => console.log(err));
-  };
-  
+  if (!mongoURI) {
+    console.error('Error: MONGO_URI is not defined in your .env file');
+    process.exit(1);
+  }
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log(process.env.MONGO_URI))
+    .then(() => console.log('MongoDB is connected'))
+    .catch(err => console.log(err));
+};
 
-// connectDB()
+// Routes de l'API
 app.use('/api/users', userRoutes);
 app.use('/api/profile', profileRoutes);
 
+// Documentation Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Démarrer le serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
